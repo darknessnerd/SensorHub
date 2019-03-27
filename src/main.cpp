fbf5ba11d2439d6a439143c157b7aaf3873cfc92
Brunino Criniti <b.criniti@ringmaster.it>
2019-03-27 15:20:38 +0100

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,51 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+#include <ESP8266HTTPUpdateServer.h>
+#include <PubSubClient.h>
+
+#ifndef STASSID
+#define STASSID "test"
+#define STAPSK  "crinitiw"
+#endif
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+const char* host = "esp8266-webupdate";
+const char* ssid = STASSID;
+const char* password = STAPSK;
+
+ESP8266WebServer httpServer(80);
+ESP8266HTTPUpdateServer httpUpdater;
+
+void setup(void) {
+
+  Serial.begin(115200);
+  Serial.println();
+  Serial.println("Booting Sketch...");
+  WiFi.mode(WIFI_AP_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    WiFi.begin(ssid, password);
+    Serial.println("WiFi failed, retrying.");
+    delay(1000);
+  }
+
+  MDNS.begin(host);
+
+  httpUpdater.setup(&httpServer);
+  httpServer.begin();
+
+  MDNS.addService("http", "tcp", 80);
+  Serial.printf("HTTPUpdateServer ready! Open http://%s.local/update in your browser\n", host);
+
+
+}
+
+void loop(void) {
+  httpServer.handleClient();
+  MDNS.update();
+}
